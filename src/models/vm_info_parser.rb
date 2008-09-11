f04de3a3778206bd896d6866628246e9e40dbdab
Dmitry Ratnikov <ratnikov@gmail.com>
2008-09-11 16:35:53 -0500

    Changed the vm info parser to be a module to parse the result of VBox vm info.
    Dmitry

--- src/models/vm_info_parser.rb
+++ src/models/vm_info_parser.rb
@@ -1,35 +1,19 @@
-class VmInfoParser
-  attr_accessor :regex, :erb_params
+module VmInfoParser
 
-  def initialize options = {}
-    template = options.delete(:template) || File.new(default_template , 'r').readlines.join
-    setup_regex_from_template template
-  end
+  FieldParsers = {
+    :name => /^Name:\s*(.+)\s*$/,
+    :ostype => /^Guest OS:\s*(.+)\s*$/
+  }
 
-  def setup_regex_from_template real_template
-    template = real_template.dup
-    regex = ""
-    @erb_params = []
-    while template.sub!(/^([^<%]*)<%([^%>]*)%>/, '') 
-      regex += "#{Regexp.escape $1}(.*)"
-      @erb_params << $2.strip.to_sym
-    end
-    @regex = /^#{regex}#{Regexp.escape template}$/
-  end
-
-  def parse str
-    if match_data = str.match(regex)
-      hash = {}
-      match_data.captures.each_index do |i|
-        hash.merge!(@erb_params[i] => match_data.captures[i])
+  def parse_vm_info raw_info
+    hash = FieldParsers.to_a.inject({}) do |acc, parser_arr|
+      unless (matches = raw_info.map { |line| line.match(parser_arr.last) }.compact).empty?
+        val = matches.map(&:captures).map(&:first).map(&:strip)
+        val = val.size == 1 ? val.first : val
+        acc.merge(parser_arr.first => val)
+      else
+        acc
       end
-      hash
-    else
-      nil
     end
-  end
-
-  def default_template
-    File.dirname(__FILE__) + '/../templates/vm_list.template.erb'
   end
 end
--- test/unit/vm_info_parser_test.rb
+++ test/unit/vm_info_parser_test.rb
@@ -1,40 +1,10 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class VmInfoParserTest < Test::Unit::TestCase
+  include VmInfoParser
 
-  def setup
-    @parser = VmInfoParser.new
-  end
-
-  def test_truth
-    assert true
-  end
-
-  def test_default_template
-    assert_equal %w(name ostype), @parser.erb_params.map(&:to_s)
-  end
-
-  def test_setup_regex_from_template
-    template = "foo: <% foo %>\nbar: <% bar %>Extra Stuff"
-    @parser.setup_regex_from_template template
-
-    assert_equal [:foo, :bar], @parser.erb_params, "The erb params should match the ones from the template."
-    assert_not_nil @parser.regex, "@parser should have regex set."
-
-    match_data = template.match @parser.regex
-    assert_not_nil match_data, "template #{template.inspect} should match the parser's new regex #{@parser.regex.inspect}."
-    @parser.erb_params.each_index do |i_param|
-      assert_equal match_data.captures[i_param], "<% #{@parser.erb_params[i_param]} %>", "Capture group #{i_param} is incorrect."
-    end
-  end
-
-  def test_parse
-    sample = File.new(File.dirname(__FILE__) + '/sample.template.erb', 'r').readlines.join
-
-    assert sample.match(@parser.regex), "The sample template should match the template regex.\nTemplate:\n#{sample}\nregex: #{@parser.regex.inspect}"
-
-    hash = @parser.parse sample
-
-    assert_equal({ :name => "Windows XP", :ostype => 'Windows'}, hash, "The machine values hash was parsed incorrectly.")
+  def test_parse_vm_info
+    hash = parse_vm_info "Name: foo\nGuest OS: Linux \nRandom stuff: blah blah\nGuest OS: Windows XP "
+    assert_equal({ :name => "foo", :ostype => [ "Linux", "Windows XP" ]}, hash, "Parsing failed...")
   end
 end
